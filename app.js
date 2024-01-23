630062f4a7774caeff9f4b589081174d26562443
miranto117 <tojo.mirantoratsimbazafy@gmail.com>
2024-01-23 22:44:01 +0100

    last commit 23/01

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-let acc = document.querySelector('ul.title-accordeon');
+let titleAcc = document.querySelector('.title-accordeon');
+
+let acc = document.querySelector('.container');
+
+
+// Ecouteur d'évenement 
+
+acc.addEventListener('click', function(event){
+    this.classList.toggle('show');
+});
+
+
+// Delegation d'evenement 
+
+acc.addEventListener('click', function(event) {
+    if (event.target.classList.contains('show')) {
+        console.log('Element cliqué');
+    }
+});
+
+
+
+
